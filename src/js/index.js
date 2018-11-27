2a9231e674c60c2e7dbb522b63689c6108c855f1
gothaw <rad.soltan@gmail.com>
2018-11-27 23:46:45 +0000

    added prototype of function to select recent projects

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ if ($('#home').length) {
         // Variables
         const valuesDescription = document.querySelectorAll('.values__description');
         const valuesTab = document.querySelectorAll('.values__tab');
-
+        const projectsMenuItems = document.querySelectorAll('.projects-menu__item');
         /**
          * @name        switchBetweenTabs
          * @param       e - tab click event
@@ -19,12 +19,42 @@ if ($('#home').length) {
             $(valuesDescription).not($targetDescription).addClass("values__description--hidden");
         }
 
+        /**
+         * @name        selectProjectCategory
+         * @desc        Function controls switching between categories and
+         * @param       e - item click event
+         */
+        function selectProjectCategory(e) {
+            const $targetItem = $(e.target);
+            $targetItem.addClass("projects-menu__item--selected");
+            $targetItem.siblings().removeClass("projects-menu__item--selected");
+            const category=$targetItem.html();
+            switch (category) {
+                case "Architecture":
+                    console.log("1");
+                    break;
+                case "Structural":
+                    console.log("2");
+                    break;
+                case "Mechanical":
+                    console.log("3");
+                    break;
+                case "Landscape":
+                    console.log("4");
+                    break;
+                default:
+                    console.log("All");
+            }
+        }
         function eventHandler() {
             for (let i=0;i<3;i++){
                 valuesTab[i].addEventListener("click",function (e) {
                         switchBetweenTabs(e,i)
                 })
             }
+            $(projectsMenuItems).on("click",function (e) {
+                selectProjectCategory(e);
+            })
         }
 
         function init() {
